d004c13a407473d0aa5db073e5954bdb0a9f1e18
santiagocpineiro <146624605+santiagocpineiro@users.noreply.github.com>
2023-11-22 13:15:37 +0100

    Updated deprecated functions

--- tools/generate_detections.py
+++ tools/generate_detections.py
@@ -4,7 +4,8 @@ import errno
 import argparse
 import numpy as np
 import cv2
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
 
 
 def _run_in_batches(f, data_dict, out, batch_size):
@@ -55,7 +56,7 @@ def extract_image_patch(image, bbox, patch_shape):
 
     # convert to top left, bottom right
     bbox[2:] += bbox[:2]
-    bbox = bbox.astype(np.int)
+    bbox = bbox.astype(int)
 
     # clip at image boundaries
     bbox[:2] = np.maximum(0, bbox[:2])
